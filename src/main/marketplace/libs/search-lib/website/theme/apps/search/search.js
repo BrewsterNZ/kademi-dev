0d1c47dcef86c3659370aae5646ce4de03c18e5a
brad <brad@kademi.co>
2018-08-03 09:25:19 +1200

    fix js init error in admin

--- src/main/marketplace/libs/search-lib/website/theme/apps/search/search.js
+++ src/main/marketplace/libs/search-lib/website/theme/apps/search/search.js
@@ -1,17 +1,20 @@
 ;(function ($) {
     $(function () {
-        var txtKeyword = $('input[name=omni]').omniSearch({
-            overrideEnterButton: false,
-            searchInFocus: true
-        });
-        
-        txtKeyword.closest('form').on('submit', function (e) {
-            var keyword = txtKeyword.val().trim();
-        
-            if (!keyword) {
-                e.preventDefault();
-            }
-        });
+        var inputs = $('input[name=omni]');
+            if( inputs.length > 0 ) {
+            var txtKeyword = inputs.omniSearch({
+                overrideEnterButton: false,
+                searchInFocus: true
+            });
+
+            txtKeyword.closest('form').on('submit', function (e) {
+                var keyword = txtKeyword.val().trim();
+
+                if (!keyword) {
+                    e.preventDefault();
+                }
+            });
+        }
     });
-    
+
 })(jQuery);
