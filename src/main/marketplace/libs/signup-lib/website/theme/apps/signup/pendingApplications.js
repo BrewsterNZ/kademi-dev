ee3b9741d638f72e343be19a1a07473c4e9ef5c0
brad <brad@kademi.co>
2018-08-06 11:25:57 +1200

    https://github.com/Kademi/projects/issues/399

--- src/main/marketplace/libs/signup-lib/website/theme/apps/signup/pendingApplications.js
+++ src/main/marketplace/libs/signup-lib/website/theme/apps/signup/pendingApplications.js
@@ -1,19 +1,20 @@
 $(function () {
     var panels = $('.panel-pending-applications');
     var isEditorPage = $(document.body).hasClass('content-editor-page');
-    
+
     if (panels.length > 0 && !isEditorPage) {
         panels.find('abbr.timeago').timeago();
-        
+
         panels.find('button.accept, button.reject').click(function (e) {
             e.preventDefault();
-            
+
             var btn = $(this);
             var isEnabled = btn.hasClass('accept');
-            
+
             if (confirm('Are you sure you want to ' + (isEnabled ? 'accept' : 'reject') + ' this application?')) {
                 var tbody = btn.closest('tbody');
                 var appId = tbody.find('input[name=appId]').val();
+                var userName = tbody.find('input[name=userName]').val();
                 var fields = tbody.find(':input');
                 var ajaxData = {
                     applicationId: appId,
@@ -24,9 +25,9 @@ $(function () {
                         ajaxData[this.name] = this.value;
                     }
                 });
-                
+
                 flog('Set status of application="' + appId + '" enabled=' + isEnabled);
-                
+
                 $.ajax({
                     type: 'POST',
                     url: '/pendingApps', // Post to orgfolder/pendingApps
@@ -38,12 +39,12 @@ $(function () {
                             Msg.error('An error occurred processing the request. Please refresh the page and try again: ' + data.messages);
                             return;
                         }
-                        
+
                         if (isEnabled) {
                             Msg.success('Accepted');
                             tbody.find('button.accept, button.reject').remove();
                             tbody.find('.btn-show-more').after(
-                                '<a href="/users/' + tbody.find('input[name=userId]').val() + '" class="btn btn-success" title="Go to profile page"><i class="fa fa-arrow-right"></i></a>'
+                                '<a href="/custs/' + tbody.find('input[name=userName]').val() + '" class="btn btn-success" title="Go to profile page"><i class="fa fa-arrow-right"></i></a>'
                             );
                         } else {
                             Msg.success('Rejected');
@@ -58,7 +59,7 @@ $(function () {
                 });
             }
         });
-        
+
         panels.find('.collapse').on({
             'hidden.bs.collapse': function (e) {
                 var target = $(this);
