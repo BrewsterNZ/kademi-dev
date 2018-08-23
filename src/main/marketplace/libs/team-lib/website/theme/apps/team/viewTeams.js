b455f4ee81ec5e989e096cdaf263998a201689d1
anh <anhnt38@gmail.com>
2018-08-23 13:55:22 +0700

    https://github.com/Kademi/kademi-dev/issues/5851

---
+++ src/main/marketplace/libs/team-lib/website/theme/apps/team/viewTeams.js
@@ -0,0 +1,34 @@
+(function ($) {
+    function initCreateMemberForm() {
+        var modal = $('#modal-add-member');
+        var form = modal.find('form');
+
+        form.forms({
+            onSuccess: function (resp) {
+                if (resp.status) {
+                    modal.modal('hide');
+                    $('#teamTable').reloadFragment();
+                    Msg.success('Member added');
+                } else {
+
+                }
+            }
+        });
+
+        $('body').on('hidden.bs.modal', '#modal-add-member', function () {
+            form.trigger('reset');
+        });
+    }
+
+    function initSelectPicker(){
+        $('.selectpicker').selectpicker({
+
+        });
+    }
+
+    $(function () {
+        initCreateMemberForm();
+        initSelectPicker();
+    });
+
+})(jQuery);
