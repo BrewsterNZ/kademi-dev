e5741e48c6b94252a9536d453a3f4f567824bedb
anh <anhnt38@gmail.com>
2018-08-23 16:40:49 +0700

    https://github.com/Kademi/kademi-dev/issues/5851

--- src/main/marketplace/libs/team-lib/website/theme/apps/team/viewTeams.js
+++ src/main/marketplace/libs/team-lib/website/theme/apps/team/viewTeams.js
@@ -3,6 +3,12 @@
         var modal = $('#modal-add-member');
         var form = modal.find('form');
 
+        form.find('#teamFinder').entityFinder({
+            type: 'organisation',
+            useActualId: true,
+            url : '/leads/'
+        });
+
         form.forms({
             onSuccess: function (resp) {
                 if (resp.status) {
@@ -26,9 +32,69 @@
         });
     }
 
+    var searchOpts = {};
+    function initSearch(){
+        var timer;
+
+        $('#team-members-query').on('input', function () {
+            clearTimeout(timer);
+            searchOpts.q = this.value;
+            timer = setTimeout(doSearch, 500);
+        });
+
+        $('#rolesFilter').on('change', function () {
+            searchOpts.role = this.value;
+            clearTimeout(timer);
+            timer = setTimeout(doSearch, 500);
+        });
+
+        $('#teamFilters').on('change', function () {
+            searchOpts.team = this.value;
+            clearTimeout(timer);
+            timer = setTimeout(doSearch, 500);
+        })
+
+    }
+
+    function doSearch(){
+        var uri = new URI(window.location.pathname);
+        for(var key in searchOpts){
+            uri.setSearch(key, searchOpts[key]);
+        }
+
+        history.pushState(null, document.title, uri.toString());
+
+        $('#searchResults').reloadFragment({
+            url: uri.toString(),
+            whenComplete: function (resp) {
+            }
+        })
+    }
+
+    function initCreateTeam(){
+        var modal = $('#modal-add-team');
+        var form = modal.find('form');
+
+        form.forms({
+            onSuccess: function (resp) {
+                if (resp && resp.status) {
+                    modal.modal('hide');
+                    $('#teamFilters').reloadFragment({
+                        whenComplete: function () {
+                            $('.selectpicker').selectpicker('refresh');
+                        }
+                    });
+                    Msg.success('Team added');
+                }
+            }
+        });
+    }
+
     $(function () {
         initCreateMemberForm();
+        initCreateTeam();
         initSelectPicker();
+        initSearch();
     });
 
 })(jQuery);
