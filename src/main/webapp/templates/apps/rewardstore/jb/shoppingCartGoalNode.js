ee77431adb09882ec46d68d9187073bda37e8f4f
brad <brad@kademi.co>
2018-08-28 16:27:34 +1200

    https://github.com/Kademi/kademi-dev/issues/5829

--- src/main/webapp/templates/apps/rewardstore/jb/shoppingCartGoalNode.js
+++ src/main/webapp/templates/apps/rewardstore/jb/shoppingCartGoalNode.js
@@ -2,7 +2,7 @@ JBNodes['shoppingCartGoal'] = {
     icon: 'fa fa-shopping-cart',
     title: 'Shopping Cart Goal',
     type: JB_NODE_TYPE.GOAL,
-
+    previewUrl: '/theme/apps/ecommerce/jb/shoppingCartGoalNode.png',
     ports: {
         timeoutNode: {
             label: 'timeout',
@@ -34,6 +34,19 @@ JBNodes['shoppingCartGoal'] = {
     settingEnabled: true,
 
     initSettingForm: function (form) {
+        form.append(
+            '<div class="form-group">' +
+            '    <div class="col-md-12">' +
+            '        <div class="checkbox">' +
+            '           <label>' +
+            '               <input type="checkbox" name="auction" class="auction">' +
+            '               Support auctions' +
+            '           </label>' +
+            '        </div>' +
+            '        <small class="text-muted help-block">If set, goal will be triggered when a user wins an auction</small>' +
+            '    </div>' +
+            '</div>' + JBApp.standardGoalSettingControls
+        );
         form.append(JBApp.standardGoalSettingControls);
 
         JBApp.initStandardGoalSettingControls(form);
@@ -41,6 +54,8 @@ JBNodes['shoppingCartGoal'] = {
         form.forms({
             allowPostForm: false,
             onValid: function () {
+                var auction = form.find('.auction').prop("checked");
+                JBApp.currentSettingNode.auction = auction || null;
                 JBApp.saveStandardGoalSetting(form);
 
                 JBApp.saveFunnel('Funnel is saved');
@@ -51,6 +66,7 @@ JBNodes['shoppingCartGoal'] = {
 
     showSettingForm: function (form, node) {
         JBApp.showStandardGoalSettingControls(form, node);
+        form.find('.auction').prop("checked", node.auction != null ? node.auction : false);
         JBApp.showSettingPanel(node);
     }
 };
