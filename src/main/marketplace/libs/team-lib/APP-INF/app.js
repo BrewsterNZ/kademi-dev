13a21ee9a50ca60da2e1d93f32ab4f0c4298bc9d
anh <anhnt38@gmail.com>
2018-09-10 17:02:39 +0700

    https://github.com/Kademi/projects/issues/562

--- src/main/marketplace/libs/team-lib/APP-INF/app.js
+++ src/main/marketplace/libs/team-lib/APP-INF/app.js
@@ -1,3 +1,4 @@
 controllerMappings.addComponent("team/components", "teamList", "html", "Shows list of teams in table format and a button for adding team member", "TeamApp");
 controllerMappings.addComponent("team/components", "teamMemberDetails", "html", "Shows team member details", "TeamApp");
+controllerMappings.addComponent("team/components", "teamMemberProfile", "html", "Shows team member profile", "TeamApp");
 controllerMappings.addComponent("team/components", "teamMemberLeads", "html", "Shows team member's assigned leads", "TeamApp");
