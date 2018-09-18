6e6076050dbd0007b0cf42500464462c6fda4f70
brad <brad@kademi.co>
2018-09-18 16:54:20 +1200

    https://github.com/Kademi/kademi-dev/issues/5994

--- src/main/webapp/templates/apps/admin/manageExtraField.js
+++ src/main/webapp/templates/apps/admin/manageExtraField.js
@@ -8,12 +8,12 @@ function initManageExtraField() {
 function initExtraField() {
     var table = $('#table-extra-field');
     var tbody = table.find('tbody');
-    
+
     tbody.find('tr').each(function () {
         var tr = $(this);
         var key = tr.find('input[name=key]').val();
         var value = tr.find('input[name=value]').val();
-        
+
         tr.html(buildExtraField(key, value));
     });
 }
@@ -25,34 +25,34 @@ function initCRUDExtraField() {
         flog("initCRUDExtraField - add field");
         openExtraFieldModal();
     });
-    
+
     var tableField = $('#table-extra-field');
-    
+
     // Edit extra field
     tableField.on('click', '.btn-edit-extra-field', function (e) {
         e.preventDefault();
-        
+
         var btn = $(this);
         var tr = btn.closest('tr');
         var key = tr.find('input[name=key]').val();
         var value = tr.find('input[name=value]').val();
-        
+
         tr.addClass('editing');
-        
+
         openExtraFieldModal(key, value);
     });
-    
+
     // Delete extra field
     tableField.on('click', '.btn-delete-extra-field', function (e) {
         e.preventDefault();
-        
+
         var btn = $(this);
-        
+
         Kalert.confirm('Do you want to remove this field?', function () {
             var fieldName = btn.attr('href');
             var fieldWrapper = btn.closest('tr');
             var groupHref = fieldWrapper.closest('table').attr('data-group-href');
-            
+
             flog('removeField', groupHref, fieldName, fieldWrapper);
             try {
                 $.ajax({
@@ -84,24 +84,24 @@ function openExtraFieldModal(key, value) {
         var txtName = $('#extra-field-name');
         var chkRequire = $('#extra-field-required');
         var chkIndexed = $('#extra-field-indexed');
-        
+
         var fieldType = $('#fieldType');
-        
+
         var txtText = $('#extra-field-text');
         var optionWrapper = $('#options-wrapper');
-        
+
         txtName.val(key);
-        
+
         var values = getValue(value, key);
-        
+
         chkRequire.prop('checked', values.require);
         flog("indexed", chkIndexed, values);
         chkIndexed.prop('checked', values.indexed);
-        
+
         fieldType.prop('value', values.fieldType);
-        
+
         txtText.val(values.text);
-        
+
         var optionString = '';
         if (values.options) {
             var options = values.options.split(',');
@@ -109,11 +109,11 @@ function openExtraFieldModal(key, value) {
                 optionString += renderOption(item);
             });
         }
-        
+
         optionWrapper.html(optionString);
         modal.addClass('edit');
     }
-    
+
     modal.modal('show');
     modal.on('hidden.bs.modal', function () {
         modal.removeClass('edit');
@@ -142,48 +142,46 @@ function initExtraFieldModal() {
     var txtText = $('#extra-field-text');
     var optionWrapper = $('#options-wrapper');
     var btnAddOption = modal.find('.btn-add-option');
-    
+
     var fieldType = $('#fieldType');
-    
+
     modal.on('hidden.bs.modal', function () {
         modal.find('input').prop('disabled', false);
         modal.find('button').removeClass('disabled');
     });
-    
+
     // Add options
     btnAddOption.on('click', function (e) {
         e.preventDefault();
-        if (fieldType.prop('value') == '') {
-            optionWrapper.append(
-                renderOption()
-            );
-        }
+        optionWrapper.append(
+            renderOption()
+        );
     });
-    
+
     fieldType.on('change', function (e) {
-        if (e.target.value != '') {
+        if (e.target.value != '' && e.target.value != 'group-sel') {
             optionWrapper.html('');
             btnAddOption.addClass('disabled');
         } else {
             btnAddOption.removeClass('disabled');
         }
     });
-    
+
     // Delete options
     optionWrapper.on('click', '.btn-delete-option', function (e) {
         e.preventDefault();
-        
+
         var btn = $(this);
-        
+
         btn.closest('.option').remove();
     });
-    
-    
+
+
     var tbody = $('#table-extra-field').find('tbody');
     var form = modal.find('form');
     var txtName = form.find('input[name=addFieldName]');
     var txtValue = form.find('input[name=addFieldValue]');
-    
+
     form.forms({
         validate: function (form, config) {
             var builtinFields = ['firstname', 'surname', 'email', 'phone', 'nickname', 'birthdate'];
@@ -194,41 +192,45 @@ function initExtraFieldModal() {
         },
         onValid: function () {
             var valueString = '';
-            
+
             // Required
             if (chkRequire.is(':checked')) {
                 valueString += 'required;';
             }
-            
+
             if (chkIndex.is(":checked")) {
                 valueString += 'indexed;';
             }
-            
+
             if ($('#fieldType').prop('value') == 'fileUpload') {
                 valueString += 'fileUpload;';
             }
-            
+
             // Options
-            if ($('#fieldType').prop('value') == 'org-sel') {
+            var fieldType = $('#fieldType').prop('value');
+            if (fieldType == 'org-sel') {
                 valueString += 'orgs();';
-            } else if ($('#fieldType').prop('value') == 'prof-id') {
+            } else if (fieldType == 'group-sel') {
+                valueString += 'groups();';
+            } else if (fieldType == 'prof-id') {
                 valueString += 'profid();';
-            } else {
-                valueString += 'options(';
-                var options = [];
-                optionWrapper.find('input:text').each(function () {
-                    var input = $(this);
-                    var optionText = input.val().trim();
-                    
-                    options.push(optionText);
-                });
-                valueString += options.join(',');
-                valueString += ');';
             }
-            
+
+            // always append options, if provided
+            valueString += 'options(';
+            var options = [];
+            optionWrapper.find('input:text').each(function () {
+                var input = $(this);
+                var optionText = input.val().trim();
+
+                options.push(optionText);
+            });
+            valueString += options.join(',');
+            valueString += ');';
+
             // Text
             valueString += 'text=' + txtText.val().trim();
-            
+
             txtValue.val(valueString);
         },
         onSuccess: function (resp) {
@@ -237,13 +239,13 @@ function initExtraFieldModal() {
                 var key = txtName.val();
                 var value = txtValue.val();
                 var newFieldString = buildExtraField(key, value);
-                
+
                 if (isEdit) {
                     tbody.find('.editing').html(newFieldString).removeClass('edit');
                 } else {
                     tbody.append('<tr>' + newFieldString + '</tr>');
                 }
-                
+
                 modal.modal('hide');
             } else {
                 Msg.error('Could not add the field. Please check your input and try again');
@@ -256,9 +258,9 @@ function getValue(value, key) {
     var values = value.split(';');
     var isRequired = false;
     var isIndexed = false;
-    
+
     var fieldType = "";
-    
+
     var text = key;
     var options = "";
     for (i = 0; i < values.length; i++) {
@@ -276,11 +278,13 @@ function getValue(value, key) {
             fieldType = "fileUpload";
         } else if (s.startsWith("orgs(")) {
             fieldType = "org-sel";
+        } else if (s.startsWith("groups(")) {
+            fieldType = "group-sel";
         } else if (s.startsWith('profid(')) {
             fieldType = "prof-id";
         }
     }
-    
+
     return {
         require: isRequired,
         text: text,
@@ -297,21 +301,21 @@ function buildExtraField(key, value) {
     var indexed = values.indexed ? '<i class="clip-notification-2 text-danger"></i>' : '';
     var text = values.text;
     var options = values.options.replace(/,/g, ', ');
-    
+
     // Key
     string += '<td>' + key;
     string += '<input type="hidden" value="' + key + '" name="key" />';
     string += '<input type="hidden" value="' + value + '" name="value" />';
     string += '</td>';
-    
+
     // Text
     string += '<td>' + text + '</td>';
-    
+
     // Required
     string += '<td>' + required + '</td>';
-    
+
     string += '<td>' + indexed + '</td>';
-    
+
     // Type
     if (values.fieldType === "fileUpload") {
         string += '<td>File Upload</td>';
@@ -319,13 +323,15 @@ function buildExtraField(key, value) {
         string += '<td>Organisation selection</td>';
     } else if (values.fieldType === 'prof-id') {
         string += '<td>Profile Identifier</td>';
+    } else if (values.fieldType === 'group-sel') {
+        string += '<td>Group selection</td>';
     } else {
         string += '<td>text</td>';
     }
-    
+
     // Options
     string += '<td>' + options + '</td>';
-    
+
     // Action
     string +=
         '<td class="action">' +
@@ -341,6 +347,6 @@ function buildExtraField(key, value) {
         '       </ul>' +
         '   </div>' +
         '</td>';
-    
+
     return string;
 }
