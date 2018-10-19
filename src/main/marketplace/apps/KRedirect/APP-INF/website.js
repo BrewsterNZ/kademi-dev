3040feabeb99e35d1e77473c95265e11ef6740ae
anh <anhnt38@gmail.com>
2018-10-19 08:56:40 +0700

    https://github.com/Kademi/kademi-dev/issues/6080

--- src/main/marketplace/apps/KRedirect/APP-INF/website.js
+++ src/main/marketplace/apps/KRedirect/APP-INF/website.js
@@ -1,19 +1,32 @@
 function kredirectResolver(rf, groupName, groupVal, mapOfGroups) {
     log.info('kredirectResolver > {} {} {} {}', rf, groupName, groupVal, mapOfGroups);
-    var website = rf.websiteName;
-    var path = '/' + groupVal;
-    var id = [website, path].join('@');
-    log.info('id {}', id);
-    var recordId = encodeURIComponent(id);
-    var db = getDB(rf);
-    var record = db.child(recordId);
-    return record;
+    var excludes = ['.css', '.js', '.png', '.jpg', '.ico', '.map', '.ttf', '.otf', '.woff2', '.woff', '.cur'];
+    var shouldCheck = true;
+    for (var i in excludes) {
+        if (groupVal.indexOf(excludes[i]) != -1) {
+            shouldCheck = false;
+            break;
+        }
+    }
+    if (shouldCheck) {
+        var website = rf.websiteName;
+        var path = '/' + groupVal;
+        var id = [website, path].join('@');
+        log.info('id {}', id);
+        var recordId = encodeURIComponent(id);
+        var db = getDB(rf);
+        var record = db.child(recordId);
+        log.info('record {}', record);
+        return record;
+    }
+    return null;
 }
 
 function redirectUrl(page, params) {
-    if (page.attributes.kredirect && page.attributes.kredirect.status && page.attributes.kredirect.targetUrl){
+    var kredirect = page.attributes.kredirect;
+    if (kredirect && kredirect.status && kredirect.targetUrl) {
         // 301 Moved Permanently
-        return views.redirectView(page.attributes.kredirect.targetUrl, true);
+        return views.redirectView(kredirect.targetUrl, true);
     }
 }
 
