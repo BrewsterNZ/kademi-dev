d80143326eda4baa1f0695eb1efc6a486aefd8d7
trungjc <trungnguyenthanh70@gmail.com>
2018-10-30 17:07:59 +0700

    Remove useless file in KookieAccept

--- src/main/marketplace/apps/KookieAccept/APP-INF/app.js
+++ src/main/marketplace/apps/KookieAccept/APP-INF/app.js
@@ -1,13 +1,6 @@
-controllerMappings.addComponent("cookie-accept/components", "cookieAccept", "html", "Add banner notice ask users for permission to use cookies", "Cookie");
-
 controllerMappings
     .websitePortletController()
     .portletSection('cookieAccept')
-    .templatePath('/theme/apps/cookie-accept/cookieAcceptPorlet.html')
-    .method('getCookieAccept')
+    .templatePath('/theme/apps/KookieAccept/cookieAcceptPorlet.html')
     .enabled(true) 
     .build();
-
-function getCookieAccept(){
-	
-}
